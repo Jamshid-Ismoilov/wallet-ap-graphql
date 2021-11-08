a9dab40415359c53b197025c85c28728cd340af1
Jamshid Ismoilov <87127098+Jamshid-Ismoilov@users.noreply.github.com>
2021-11-08 18:14:19 +0500

    Update config.go

--- app/tools/config.go
+++ app/tools/config.go
@@ -1,6 +1,6 @@
 package tools
 
-var GSender = "gamerdeveloper1408@gmail.com"
-var GPassword = "gamerdev.1.1.1"
+var GSender = "example@gmail.com"
+var GPassword = "example"
 var GHost = "smtp.gmail.com"
 var GPort = "587"
